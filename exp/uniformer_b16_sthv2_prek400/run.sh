fc7099dcfc593ba1183dff070f933d09df0a117c
Harry-Hu <47730076+JieHarry-Hu@users.noreply.github.com>
2023-03-24 19:44:41 +0800

    Add files via upload

---
+++ exp/uniformer_b16_sthv2_prek400/run.sh
@@ -0,0 +1,22 @@
+work_path=$(dirname $0)
+PYTHONPATH=$PYTHONPATH:./slowfast \
+python tools/run_net.py \
+  --cfg $work_path/config.yaml \
+  DATA.PATH_TO_DATA_DIR ./data_list/sthv2 \
+  DATA.PATH_PREFIX ./data_list/sthv2/extracted_frames \
+  DATA.LABEL_PATH_TEMPLATE "somesomev2_rgb_{}_split.txt" \
+  DATA.IMAGE_TEMPLATE "{:06d}.jpg" \
+  DATA.PATH_LABEL_SEPARATOR "," \
+  TRAIN.EVAL_PERIOD 2 \
+  TRAIN.CHECKPOINT_PERIOD 1 \
+  TRAIN.BATCH_SIZE 8 \
+  NUM_GPUS 1 \
+  UNIFORMER.DROP_DEPTH_RATE 0.4 \
+  SOLVER.MAX_EPOCH 50 \
+  SOLVER.BASE_LR 5e-4 \
+  SOLVER.WARMUP_EPOCHS 5.0 \
+  DATA.TEST_CROP_SIZE 224 \
+  TEST.NUM_ENSEMBLE_VIEWS 1 \
+  TEST.NUM_SPATIAL_CROPS 3 \
+  RNG_SEED 6666 \
+  OUTPUT_DIR $work_path
---
+++ exp/uniformer_s16_sthv2_prek600/test.sh
@@ -0,0 +1,21 @@
+work_path=$(dirname $0)
+PYTHONPATH=$PYTHONPATH:./slowfast \
+python tools/run_net.py \
+  --cfg $work_path/test.yaml \
+  DATA.PATH_TO_DATA_DIR ./data_list/sthv2 \
+  DATA.PATH_PREFIX ./data_list/sthv2/extracted_frames \
+  DATA.PATH_LABEL_SEPARATOR "," \
+  TRAIN.EVAL_PERIOD 5 \
+  TRAIN.CHECKPOINT_PERIOD 1 \
+  TRAIN.BATCH_SIZE 16 \
+  NUM_GPUS 1 \
+  UNIFORMER.DROP_DEPTH_RATE 0.2 \
+  SOLVER.MAX_EPOCH 20 \
+  SOLVER.BASE_LR 2e-4 \
+  SOLVER.WARMUP_EPOCHS 5.0 \
+  DATA.TEST_CROP_SIZE 224 \
+  TEST.NUM_ENSEMBLE_VIEWS 1 \
+  TEST.NUM_SPATIAL_CROPS 3 \
+  TRAIN.ENABLE False \
+  TEST.CHECKPOINT_FILE_PATH ./exp/uniformer_s16_sthv2_prek600/checkpoints/checkpoint.pyth \
+  OUTPUT_DIR $work_path
